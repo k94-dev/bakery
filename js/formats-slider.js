bf165e413817297b70a56412e0545fa6afbad1ea
k94-dev <sergeykom70@gmail.com>
2025-02-04 16:32:28 +0200

    1
    1

---
+++ js/formats-slider.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        480: {
+          slidesPerView: 1,
+    },
+        768: {
+          slidesPerView: 2,
+    },
+        1280: {
+          slidesPerView: 3,
+    }
+  },
+   pagination: {
+       el: '.pagination',
+       bulletClass: 'pagination__button',
+       bulletActiveClass: 'pagination__button--active'
+  },
+   navigation: {
+    nextEl: '.carousel-button.right',
+    prevEl: '.carousel-button.prev',
+  },
+});
